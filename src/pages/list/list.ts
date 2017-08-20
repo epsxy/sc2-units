61a50a2b39bff379b4aae22770c9f0f843af5b4d
epsxy <epsxy@users.noreply.github.com>
2017-08-20 16:46:47 +0200

    optimize imports

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,13 +1,9 @@
-import { HomePage } from '../home/home';
 import { NotFoundPage } from '../404NotFound/404';
 import { SC2_RACES } from '../../data/races/races';
-import { PROTOSS_UNITS } from '../../data/units/protoss';
-import { ZERG_UNITS } from '../../data/units/zerg';
-import { TERRAN_UNITS } from '../../data/units/terran';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-import { SC2Race, SC2RaceType } from '../../model/sc2races';
+import { SC2Race } from '../../model/sc2races';
 import { SC2Unit } from '../../model/sc2unit';
 
 @Component({
