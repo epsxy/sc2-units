221fae0f364333ee0429876b8668c6b2c42b927f
epsxy <epsxy@users.noreply.github.com>
2017-08-21 15:37:15 +0200

    Add information images & handle different images for each race in unit page

--- src/pages/unit/unit.ts
+++ src/pages/unit/unit.ts
@@ -1,28 +1,51 @@
+import { NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core';
 import { NotFoundPage } from '../404NotFound/404';
 import { SC2_UNITS } from '../../data/units/units';
 import { SC2Unit } from '../../model/sc2unit';
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { SC2RaceType } from '../../model/sc2races';
 
 @Component({
   selector: 'page-unit',
   templateUrl: 'unit.html'
 })
 export class UnitPage {
-  id: string;
-  unit : SC2Unit;
   nav: NavController;
+  id: string;
+  unit: SC2Unit;
+  minerals_img: string;
+  gas_img: string;
+  buildtime_img: string;
+  supply_img: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.nav = navCtrl
     this.id = this.navParams.get('id');
     console.log(this.id);
-    let unitsMatchingId = SC2_UNITS.filter(unit =>unit.id == this.id);
-    if(unitsMatchingId.length != 1) {
+    let unitsMatchingId = SC2_UNITS.filter(unit => unit.id == this.id);
+    if (unitsMatchingId.length != 1) {
       this.nav.setRoot(NotFoundPage);
     }
     else {
       this.unit = unitsMatchingId[0];
+      if (this.unit.race.type == SC2RaceType.PROTOSS) {
+        this.minerals_img = 'assets/icon/ressources/protoss/minerals.gif';
+        this.gas_img = 'assets/icon/ressources/protoss/gas.gif';
+        this.buildtime_img = 'assets/icon/ressources/protoss/buildtime.gif';
+        this.supply_img = 'assets/icon/ressources/protoss/supply.gif';
+      }
+      else if (this.unit.race.type == SC2RaceType.ZERG) {
+        this.minerals_img = 'assets/icon/ressources/zerg/minerals.gif';
+        this.gas_img = 'assets/icon/ressources/zerg/gas.gif';
+        this.buildtime_img = 'assets/icon/ressources/zerg/buildtime.gif';
+        this.supply_img = 'assets/icon/ressources/zerg/supply.gif';
+      }
+      else {
+        this.minerals_img = 'assets/icon/ressources/terran/minerals.gif';
+        this.gas_img = 'assets/icon/ressources/terran/gas.gif';
+        this.buildtime_img = 'assets/icon/ressources/terran/buildtime.gif';
+        this.supply_img = 'assets/icon/ressources/terran/supply.gif';
+      }
     }
   }
 }
