c4855bba218d0b9e1c12cd30be69e6ec52fa3918
epsxy <epsxy@users.noreply.github.com>
2017-08-21 17:18:28 +0200

    Update Asset types

---
+++ src/model/sc2asset.ts
@@ -0,0 +1,10 @@
+
+export class SC2Asset {
+    thumbnail: string;
+    image: string;
+
+    constructor(thumbnail: string, image: string) {
+        this.thumbnail = thumbnail;
+        this.image = image;
+    }
+}
